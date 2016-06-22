f3b7485cf0c853f9aeb4c24a71c1d2c654da60ce
Luke Burns <lukemburns@gmail.com>
2016-06-22 10:31:50 -0400

    corrections now that all streams are readable

--- index.js
+++ index.js
@@ -6,26 +6,21 @@ module.exports = function swarmStream (key, opts) {
 
   var stream = createStream(key, opts)
 
-  var readable = !!stream.read
   var writable = !!stream.write
 
-  if (readable) {
-    stream.swarm = createSwarm(stream.feed)
-    stream.on('end', function () {
-      if (opts.exit && stream.swarm) stream.swarm.node.close()
-      stream.emit('exit')
-    })
-  }
+  stream.swarm = createSwarm(stream.feed)
+  stream.on('end', function () {
+    if (opts.exit && stream.swarm) stream.swarm.node.close()
+    stream.emit('exit')
+  })
 
   if (writable) {
     if (opts.static) {
       stream.on('finish', function () {
-        if (!opts.exit) stream.swarm = stream.swarm || createSwarm(stream.feed)
-        else if (stream.swarm) stream.swarm.node.close()
+        if (stream.swarm) stream.swarm.node.close()
         stream.emit('exit')
       })
     } else {
-      stream.swarm = stream.swarm || createSwarm(stream.feed)
       stream.on('finish', function () {
         if (opts.exit && stream.swarm) stream.swarm.node.close()
         stream.emit('exit')
